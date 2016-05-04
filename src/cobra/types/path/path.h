346fab7f7c6a62525a004e544d0af504479995c9
= <chase.willden@gmail.com>
2016-05-03 18:42:22 -0600

    updated license info

--- src/cobra/types/path/path.h
+++ src/cobra/types/path/path.h
@@ -1,3 +1,7 @@
+// Copyright 2016 Chase Willden and The CobraLang Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 #ifndef PATH_H_
 #define PATH_H_
 
